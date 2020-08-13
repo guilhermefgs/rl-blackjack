c861088cdc63b3d480a9a8d5302e2f178e7aadc6
Guilherme Fernandes <aateg@live.com>
2020-08-13 15:17:25 -0300

    add test agent function

--- main.py
+++ main.py
@@ -1,45 +1,64 @@
 import gym
-from LogicAgent import LogicAgent
+from agents import LogicAgent, QAgent
 
-def print_game(observation=None, reward=None, action=None, i_episode=None):
+# TODO 
+# create test_agent for any agent
+# create base agent
+# create pandas dataframe for game log
+
+def print_game(observation=None, reward=None, action=None, i_episode=None, current_reward=None):
     """Void function for printing the game
     Args:
         - observation: (tuple) the state of the game
         - reward: (double) the reward of the game
         - action: (boolean) last action taken
-        - episode: (int) game round
+        - i_episode: (int) game round
+        - current_reward: (double) current table player's reward
     """
     print(f"Game Round - {i_episode}")
     print("Action: ", action)
     print("State: ", observation)
     print("Reward: ", reward)
+    print("Current Game Reward: ", current_reward)
+    print("\n")
 
-if __name__ == '__main__':
-
-    # create environment
-    env = gym.make('Blackjack-v0')
-    agent = LogicAgent()
-    cumulative_reward = 0
-
+def test_agent(agent, env, n_episodes=500):
+    """Function that test the agent
+    Args:
+        - agent: (Agent) the player
+        - env: (gym.env) the environment/ the game
+        - n_episodes: (int) the number
+    Returns:
+    """
+    final_reward = 0
     # simulate random Agente
-    for i_episode in range(20):
+    for i_episode in range(n_episodes):
 
         observation = env.reset()
         print_game(observation, i_episode)
-        
+        current_reward = 0 
         for t in range(100):
             
-            action = agent.action(observation)
-
+            action = agent.choose_action(observation)
             observation, reward, done, info = env.step(action)
-            print_game(observation, reward, action, i_episode)
-            cumulative_reward += reward
+
+            current_reward += reward
+            print_game(observation, reward, action, i_episode, current_reward)
             
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 print("\n")
                 break
 
-        print("Current Reward: ", cumulative_reward)
-    print("Final Reward: ", cumulative_reward)
+        final_reward += current_reward
+        print("Final Reward: ", final_reward)
     env.close()
+
+
+if __name__ == '__main__':
+
+    # create environment
+    env = gym.make('Blackjack-v0')
+    agent = LogicAgent()
+
+    test_agent(agent, env)
